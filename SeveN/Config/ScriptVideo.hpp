3d08ed29591558e6a50605924ccbcb5a21783b17
Matheus Evandro Rech <50255753+MatheusRech@users.noreply.github.com>
2021-03-16 13:09:27 -0300

    Update class video

--- SeveN/Config/ScriptVideo.hpp
+++ SeveN/Config/ScriptVideo.hpp
@@ -9,8 +9,8 @@ class Video {
 
 	//Somente adicionar videos daqui para baixo usando o exemplo!!!!
 
-	class Exemplo {
-		nome = "Meu video"; // Nome do video
+	class DiaD {
+		nome = "Dia D"; // Nome do video
 		path = "\SeveN\Video\meuvideo.ogv"; // Local do arquivo a partir da pasta da missão
 	}
 }
